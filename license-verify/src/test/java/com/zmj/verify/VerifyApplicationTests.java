116add9b79d489b1271a6fd5aaab54997ae74820
zhangmingji <17621505328@163.com>
2020-03-13 10:21:10 +0800

    license初始化结构

---
+++ license-verify/src/test/java/com/zmj/verify/VerifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zmj.verify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VerifyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
